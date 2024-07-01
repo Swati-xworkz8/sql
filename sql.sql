b840cc9812daa601fcd888732065029c6e2a7c8c
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-07-01 10:16:26 +0530

    sql query 1/7/2024

---
+++ sql.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE xworkz;
+CREATE TABLE ground(id int,ground_name varchar(10),ground_size int);
+CREATE DATABASE roop;
+CREATE TABLE mail(id int,mail_name varchar(10),mail_id varchar(20));
+CREATE TABLE accessiors(id int, accessiors_name varchar(10),);
